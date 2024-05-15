f5564accd497df2fc911dbdf5ab71f8faadcad85
Adnan Alam <adnanalamcie@gmail.com>
2024-05-15 18:08:45 +1000

    Posting loading states and toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { GeistSans } from "geist/font/sans";
 import { type AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "react-hot-toast";
 
 import { api } from "~/utils/api";
 
@@ -10,6 +11,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider>
       <main className={GeistSans.className}>
+        <Toaster position="bottom-center" />
         <Component {...pageProps} />
       </main>
     </ClerkProvider>
