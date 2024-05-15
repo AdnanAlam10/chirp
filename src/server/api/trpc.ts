10c53045da5f1f303201680ccaae7ea9674fe739
Adnan Alam <adnanalamcie@gmail.com>
2024-05-15 15:43:58 +1000

    Ratelimit the endpoint

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -6,10 +6,9 @@
  * TL;DR - This is where all the tRPC server stuff is created and plugged in. The pieces you will
  * need to use are documented accordingly near the end.
  */
-import { Session, getAuth } from "@clerk/nextjs/server";
+import { getAuth } from "@clerk/nextjs/server";
 import { TRPCError, initTRPC } from "@trpc/server";
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
-import next from "next";
 import superjson from "superjson";
 import { ZodError } from "zod";
 
