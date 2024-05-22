cf90be026fe99305df307e30578bb7d0fdd9ed63
Adnan Alam <adnanalamcie@gmail.com>
2024-05-22 17:55:49 +1000

    separate ssg helper, full post page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -2,14 +2,11 @@
 import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { api } from "~/utils/api";
-import { createServerSideHelpers } from "@trpc/react-query/server";
-import { appRouter } from "~/server/api/root";
-import { db } from "~/server/db";
-import SuperJSON from "superjson";
 import { PageLayout } from "~/components/layout";
 import Image from "next/image";
 import { LoadingPage } from "~/components/loading";
 import { PostView } from "~/components/postview";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 
 const ProfileFeed = (props: { userId: string }) => {
   const { data, isPending } = api.posts.getPostsByUserID.useQuery({
@@ -61,11 +58,7 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const ssg = createServerSideHelpers({
-    router: appRouter,
-    ctx: { db, userId: null },
-    transformer: SuperJSON, // optional - adds superjson serializer
-  });
+  const ssg = generateSSGHelper();
 
   const slug = context.params?.slug;
 
